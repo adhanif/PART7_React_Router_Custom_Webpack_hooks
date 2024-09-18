71c9f15ed334074eac7cbb50b28246ec71e01b44
ad.hanif <ad.hanif@outlook.com>
2024-09-18 13:48:47 +0300

    Routed Anecdotes, step 1

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from 'react';
+
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Anecdotes from './anecdotes/Anecdotes';
+import Menu from './menu/Menu';
+import Footer from './footer/Footer';
+import CreateAnecdote from './createAnecdote/CreateAnecdote';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Anecdotes />}></Route>
+        <Route path='/create' element={<CreateAnecdote />}></Route>
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ routed-anecdotes/src/anecdotes/Anecdotes.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Anecdotes() {
+  return <h1>Anecdotes</h1>;
+}
+
+export default Anecdotes;
